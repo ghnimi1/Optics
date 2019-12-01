3018ee2bf60b1fb43f59a992dd8a641165b0a93c
ghnimi1 <ghnimih@gmail.com>
2019-12-01 11:10:15 +0100

    update

--- src/component/Team/index.js
+++ src/component/Team/index.js
@@ -7,25 +7,25 @@ const Team = () => {
                    <TeamTitle>Meet the Team</TeamTitle>
                        <TitleBar></TitleBar>
                        <Part active>
-                           <Image src="/images/Portfolio/portfolio-img1.jpg"></Image>
+                           <Image src="./images/Portfolio/portfolio-img1.jpg"></Image>
                            
                            <Title>John Doe</Title>
                            <Desc>Web developer </Desc>
                        </Part>
                        <Part>
-                           <Image src="/images/Portfolio/portfolio-img2.jpg"></Image>
+                           <Image src="./images/Portfolio/portfolio-img2.jpg"></Image>
                            
                            <Title>John Doe</Title>
                            <Desc>Web developer </Desc>
                        </Part>
                        <Part>
-                           <Image src="/images/Portfolio/portfolio-img3.jpg"></Image>
+                           <Image src="./images/Portfolio/portfolio-img3.jpg"></Image>
                            
                            <Title>John Doe</Title>
                            <Desc>Web developer </Desc>
                        </Part>
                        <Part>
-                           <Image src="/images/Portfolio/portfolio-img4.jpg"></Image>
+                           <Image src="./images/Portfolio/portfolio-img4.jpg"></Image>
                            
                            <Title>John Doe</Title>
                            <Desc>Web developer </Desc>
